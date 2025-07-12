9636151e6e70c0ac44497821debad5dec0c68829
Mohamed Hassanin <mohamed.hassanin.250@gmail.com>
2025-07-12 05:57:37 +0300

    Calculate personal finance

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,10 +1,10 @@
-income = float(input("Enter your income: "))
+income = float(input("Enter your monthly income: "))
 
 mo_expenses = float(input("Enter your total monthly expenses: "))
 
 mo_saving = income - mo_expenses
 
-annual_interset = 0.05
+annual_interest = 0.05
 
 projected_saving = (mo_saving *12) + (mo_saving * 12 * 0.05)
 
