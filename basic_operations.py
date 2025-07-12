79255e2b804d66448dd9c778a0b18774fa63e6f6
Mohamed Hassanin <mohamed.hassanin.250@gmail.com>
2025-07-12 03:11:48 +0300

    My first python code

--- basic_operations.py
+++ basic_operations.py
@@ -1,10 +1,10 @@
 number1 = 10
 number2 = 5
 
-sum = number1 + number2
+add = number1 + number2
 dif = number1 - number2
 pro = number1 * number2
 
-print ("Addition of " + str( number1 )+ " and " + str(number2) + " is " + str(sum))
+print ("Addition of " + str( number1 )+ " and " + str(number2) + " is " + str(add))
 print ("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(dif))
 print ("Multiplication of " + str(number1) + " and " + str(number2) +" is " + str(pro))
